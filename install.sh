7fa8f045c4b8f841b738e2f1d21f7854054cda9d
Sergio Vicente de las Heras <6536180+echosergio@users.noreply.github.com>
2025-05-09 10:42:06 +0200

    Use custom Git Environment Variables

--- install.sh
+++ install.sh
@@ -16,3 +16,9 @@ source ~/.bashrc
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' & disown
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' & disown # Yep, twice
 nvim --headless -c 'TSUpdateSync' & disown
+
+# Custom Git Environment Variables
+export GIT_COMMITTER_NAME="Sergio Vicente de las Heras"
+export GIT_COMMITTER_EMAIL="6536180+echosergio@users.noreply.github.com"
+export GIT_AUTHOR_NAME="Sergio Vicente de las Heras"
+export GIT_AUTHOR_EMAIL="6536180+echosergio@users.noreply.github.com"
